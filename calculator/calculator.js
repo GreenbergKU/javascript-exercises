c148b453e4d87535b18fa89accdd535643f281c9
Suzie Q <suzieqwolf@gmail.com>
2020-07-14 01:41:36 -0600

    Adds solutions/code for calculator exercise.

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,58 @@
-function add () {
-	
+function add(...numbers) {
+	let addNumSum = 0;
+	numbers.forEach(function (number) {
+		addNumSum += number;
+	});
+	return addNumSum;
 }
 
-function subtract () {
-	
+function subtract(...numbers) {
+	let subNum = null;
+	numbers.forEach(function (number) {
+		subNum = subNum === null ? number : subNum - number;
+	});
+	return subNum;
 }
 
-function sum () {
-	
+function sum(sumNumbers) { 
+	let sum = 0;
+	sumNumbers.forEach(function(sumNumber) {
+		sum += sumNumber;
+	});
+	return sum;	
 }
 
-function multiply () {
-	
+// function sum(numbersArr) {
+// 	let numbers = "";
+// 	numbersArr.forEach(function(number) {	
+// 		numbers += number === "" ? `${number}` : `, ${number}`;
+// 		console.log('numbers: ', numbers);
+// 	});
+// 	add(numbers)
+
+// }
+
+function multiply(numbers) {
+	let product = null;
+	numbers.forEach(function(number) {
+		product = product === null ? number : product * number;	
+	});
+	// console.log('product: ', product);
+	return product;	
 }
 
-function power() {
-	
+function power(num1, num2) {
+	const powerNum = num1 ** num2;
+	// const powerNum = Math.pow(num1, num2);
+	return powerNum;
 }
 
-function factorial() {
+function factorial(num) {
+	return num ? num * factorial(num - 1) : 1;
 	
+	// let factorialNum = num ? num * factorial(num - 1) : 1;	
+	// console.log('num!: ', factorialNum);		
+	// return factorialNum 			
 }
 
 module.exports = {
@@ -30,3 +63,18 @@ module.exports = {
     power,
 	factorial
 }
+
+/*
+JASMINE TEST: TERMINAL
+	jasmine calculator.spec.js
+
+EXERCISES:
+X	calculator
+	palindromes
+	caesar
+	fibonacci
+	getTheTitles
+	findTheOldest
+		
+	(answers = ‘solutions’ branch of repo)
+*/
