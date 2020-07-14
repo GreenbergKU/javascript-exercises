bdaee45762863aa8e12067bd4acdf1c94ab31002
Suzie Q <suzieqwolf@gmail.com>
2020-07-14 01:56:12 -0600

    Adds comments: Directions for exercises.

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -5,10 +5,20 @@ const fibonacci = function() {
 module.exports = fibonacci
 
 /*
-JASMINE TEST: TERMINAL
+//// JASMINE TEST: TERMINAL ////
+
 	jasmine fibonacci.spec.js
 
-EXERCISES:
+
+//// DIRECTIONS ////
+
+- Create a function that returns a specific member of the fibonacci sequence:
+
+    > a series of numbers in which each number ( Fibonacci number ) is the sum of the two preceding numbers. The simplest is the series 1, 1, 2, 3, 5, 8, etc.
+
+
+//// EXERCISES ////
+
 X	calculator
 	palindromes
 	caesar
--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -5,11 +5,20 @@ const palindromes = function() {
 module.exports = palindromes
 
 /*
-JASMINE TEST: TERMINAL
+//// JASMINE TEST: TERMINAL ////
+
 	jasmine palindromes.spec.js
 
 
-EXERCISES:
+//// DIRECTIONS ////
+
+- Write a function that determines whether or not a given string is a palindrome.
+
+- A palindrome is a string that is spelled the same both forwards and backwards, usually without considering punctuation or word breaks:
+
+
+//// EXERCISES ////
+
 X	calculator
 	palindromes
 	caesar
